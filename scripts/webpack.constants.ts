24ac152759b6b3b26fafb189907da53e90751a48
Stephen Li <stephenli.ca@gmail.com>
2021-01-19 00:32:12 -0500

    Move dist-* to subfolders of common dist dir

--- scripts/webpack.constants.ts
+++ scripts/webpack.constants.ts
@@ -8,9 +8,10 @@ const rootDir = path.resolve() // Assume we are running from the project root
 export const scriptsDir = path.resolve(rootDir, 'scripts')
 export const staticDir = path.resolve(rootDir, 'static')
 
-export const distCronDir = path.resolve(rootDir, 'dist-cron')
-export const distClientDir = path.resolve(rootDir, 'dist-web-client')
-export const distServerDir = path.resolve(rootDir, 'dist-web-server')
+const distDir = path.resolve(rootDir, 'dist')
+export const distCronDir = path.resolve(distDir, 'cron')
+export const distClientDir = path.resolve(distDir, 'web-client')
+export const distServerDir = path.resolve(distDir, 'web-server')
 
 export const srcDir = path.join(rootDir, 'src')
 export const srcCronDir = path.resolve(srcDir, 'cron')
