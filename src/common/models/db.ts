90b5f3c21049dd18ef9957829d866f3eea6843ce
Stephen Li <stephenli.ca@gmail.com>
2021-01-23 20:10:08 -0500

    Store db host in secrets

--- src/common/models/db.ts
+++ src/common/models/db.ts
@@ -3,12 +3,13 @@ import mysql from 'mysql2'
 
 import { getSecret, Secrets } from '@common/Secrets'
 
+const host = getSecret(Secrets.MYSQL_HOST)
 const db = getSecret(Secrets.MYSQL_DATABASE)
 const user = getSecret(Secrets.MYSQL_USER)
 const pass = getSecret(Secrets.MYSQL_PASSWORD)
 
 const sequelize = new Sequelize(db, user, pass, {
-    host: 'db',
+    host: host,
     dialect: 'mysql',
     dialectModule: mysql,
 })
