25b48865a0860f16149212c58a675ae36c5f5330
Stephen Li <stephenli.ca@gmail.com>
2021-01-26 01:06:21 -0500

    Prioritize cmd arg first in migration script

--- db/run.sh
+++ db/run.sh
@@ -7,6 +7,8 @@ PASSWORD=`cat /run/secrets/MYSQL_PASSWORD`
 
 export DATABASE_URL="mysql://${USER}:${PASSWORD}@${HOST}/${DATABASE}"
 
-dbmate --migrations-dir ./migrations up
+dbmate --migrations-dir ./migrations ${1:-'up'} || true
+
+echo "Finished Migration"
 
 read -n 1 -s -r -p "Press any key to continue"; echo
