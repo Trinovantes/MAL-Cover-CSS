6b95ae039a20c01c656077a2f0c12bfeea3a975d
Stephen Li <stephenli.ca@gmail.com>
2021-01-27 19:08:07 -0500

    Fix missing env var in backup script

--- db/backup.sh
+++ db/backup.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-mysqldump -h ${HOST} -u ${USER} --all-databases --single-transaction > "./backups/$(date --utc +%FT%TZ).sql"
+HOST=`cat /run/secrets/MYSQL_HOST`
+USER=`cat /run/secrets/MYSQL_USER`
+PASSWORD=`cat /run/secrets/MYSQL_PASSWORD`
+
+export MYSQL_PWD=${PASSWORD}
+
+mysqldump -h ${HOST} -u ${USER} --single-transaction --all-databases > "./backups/$(date --utc +%FT%TZ).sql"
