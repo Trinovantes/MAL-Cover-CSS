a13b70b8c0ff7945b169e2bf17675d8c6b54712c
Stephen Li <stephenli.ca@gmail.com>
2022-03-21 23:47:40 -0400

    Upgrade dependencies
    - Register express handlers inside functions
    
        Since redisClient.connect() now returns a promise, we need to wait
        for it to finish before we can register the session middleware and
        the subsequent middlewares that follow it
    
        Setting middlewares via normal/async functions keeps everything
        consistent and clean inside runHttpServer()
    
    - Move monokai.css import to main.scss
    
        No idea why vue-loader doesn't register monokai.css as a vendor css
        from inside component anymore
    
        Not worth the headache to debug so we just register it globally
    
    - Pin node-gyp to v9
    
        sqlite3 package requires node-gyp but it hasn't been updated in ages

--- src/api/www.ts
+++ src/api/www.ts
@@ -1,8 +1,8 @@
-import ConnectRedis from 'connect-redis'
+import connectRedis from 'connect-redis'
 import express, { ErrorRequestHandler } from 'express'
 import http from 'http'
 import morgan from 'morgan'
-import redis from 'redis'
+import { createClient } from 'redis'
 import session from 'express-session'
 import { getSecret, isHttpsEnabled, Secrets } from '@/common/utils/secrets'
 import { COOKIE_DURATION, SENTRY_DSN } from '@/common/Constants'
@@ -33,109 +33,133 @@ app.set('trust proxy', trustProxy)
 // Sentry
 // -----------------------------------------------------------------------------
 
-Sentry.init({
-    dsn: SENTRY_DSN,
-    release: DEFINE.GIT_HASH,
-    integrations: [
-        // Enable HTTP calls tracing
-        new Sentry.Integrations.Http({ tracing: true }),
-        // Enable Express.js middleware tracing
-        new Integrations.Express({
-            // To trace all requests to the default router
-            app,
-            // Alternatively, you can specify the routes you want to trace:
-            // router: someRouter,
-        }),
-    ],
+function setupSentry() {
+    Sentry.init({
+        dsn: SENTRY_DSN,
+        release: DEFINE.GIT_HASH,
+        integrations: [
+            // Enable HTTP calls tracing
+            new Sentry.Integrations.Http({ tracing: true }),
+            // Enable Express.js middleware tracing
+            new Integrations.Express({
+                // To trace all requests to the default router
+                app,
+                // Alternatively, you can specify the routes you want to trace:
+                // router: someRouter,
+            }),
+        ],
 
-    // We recommend adjusting this value in production, or using tracesSampler for finer control
-    tracesSampleRate: 1.0,
-    enabled: !DEFINE.IS_DEV,
-})
+        // We recommend adjusting this value in production, or using tracesSampler for finer control
+        tracesSampleRate: 1.0,
+        enabled: !DEFINE.IS_DEV,
+    })
 
-// RequestHandler creates a separate execution context using domains, so that every transaction/span/breadcrumb is attached to its own Hub instance
-app.use(Sentry.Handlers.requestHandler())
+    // RequestHandler creates a separate execution context using domains, so that every transaction/span/breadcrumb is attached to its own Hub instance
+    app.use(Sentry.Handlers.requestHandler())
 
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler())
+    // TracingHandler creates a trace for every incoming request
+    app.use(Sentry.Handlers.tracingHandler())
+}
 
 // -----------------------------------------------------------------------------
 // Middlewares
 // -----------------------------------------------------------------------------
 
 // Handle non-GET request bodies
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+function setupBodyParsers() {
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: false }))
+}
 
-// Logging
-app.use(morgan(DEFINE.IS_DEV ? 'dev' : 'combined'))
+function setupLogging() {
+    // Logging
+    app.use(morgan(DEFINE.IS_DEV ? 'dev' : 'combined'))
+}
 
-// Sessions
-console.info(`Setting session secure:${isHttpsEnabled()} REDIS_HOST:${getSecret(Secrets.REDIS_HOST)} REDIS_PORT:${getSecret(Secrets.REDIS_PORT)}`)
-app.use(session({
-    secret: getSecret(Secrets.ENCRYPTION_KEY),
-    resave: false,
-    saveUninitialized: false,
-    proxy: trustProxy,
-    cookie: {
-        maxAge: COOKIE_DURATION,
-        httpOnly: true, // Cookies will not be available to Document.cookie api
-        secure: isHttpsEnabled(), // Cookies only sent over https
-    },
-    store: (() => {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        const RedisStore = ConnectRedis(session)
-        const redisClient = redis.createClient({
+async function setupSession() {
+    console.info(`Setting session secure:${isHttpsEnabled()} REDIS_HOST:${getSecret(Secrets.REDIS_HOST)} REDIS_PORT:${getSecret(Secrets.REDIS_PORT)}`)
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const RedisStore = connectRedis(session)
+    const redisClient = createClient({
+        legacyMode: true,
+        socket: {
             host: getSecret(Secrets.REDIS_HOST),
             port: parseInt(getSecret(Secrets.REDIS_PORT)),
-        })
-        return new RedisStore({ client: redisClient })
-    })(),
-}))
+        },
+    })
+
+    await redisClient.connect()
+
+    app.use(session({
+        secret: getSecret(Secrets.ENCRYPTION_KEY),
+        resave: false,
+        saveUninitialized: false,
+        proxy: trustProxy,
+        cookie: {
+            maxAge: COOKIE_DURATION,
+            httpOnly: true, // Cookies will not be available to Document.cookie api
+            secure: isHttpsEnabled(), // Cookies only sent over https
+        },
+        store: new RedisStore({ client: redisClient }),
+    }))
+}
 
 // -----------------------------------------------------------------------------
 // Request Handler
 // -----------------------------------------------------------------------------
 
 // Serve API endpoints
-app.use('/api/oauth', debugInfo, setUserInLocals, oauthRouter)
-app.use('/api/settings', debugInfo, setUserInLocals, settingsRouter)
-app.use('/api', (req, res, next) => {
-    next(new createHttpError.NotFound())
-})
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const errorHander: ErrorRequestHandler = (err, req, res, next) => {
-    if (axios.isAxiosError(err)) {
-        // Don't log entire Axios err object since it includes req, res
-        console.info(err.name, err.message)
-    } else if (!(err instanceof HttpError)) {
-        // Log unexpected non-HttpErrors
-        console.info(err)
-    }
-
-    // Set locals, only providing error in development
-    const status = err instanceof HttpError ? err.status : 500
-    const message = err instanceof HttpError ? err.message : 'Internal Server Error'
-
-    const response: ErrorResponse = {
-        errorMessage: message,
-    }
-
-    res.status(status)
-    res.json(response)
+function setupApiResponseHandlers() {
+    app.use('/api/oauth', debugInfo, setUserInLocals, oauthRouter)
+    app.use('/api/settings', debugInfo, setUserInLocals, settingsRouter)
+    app.use('/api', (req, res, next) => {
+        next(new createHttpError.NotFound())
+    })
 }
 
-app.use(Sentry.Handlers.requestHandler())
-app.use(errorHander)
+function setupErrorResponseHandlers() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const errorHander: ErrorRequestHandler = (err, req, res, next) => {
+        if (axios.isAxiosError(err)) {
+            // Don't log entire Axios err object since it includes req, res
+            console.info(err.name, err.message)
+        } else if (!(err instanceof HttpError)) {
+            // Log unexpected non-HttpErrors
+            console.info(err)
+        }
+
+        // Set locals, only providing error in development
+        const status = err instanceof HttpError ? err.status : 500
+        const message = err instanceof HttpError ? err.message : 'Internal Server Error'
+
+        const response: ErrorResponse = {
+            errorMessage: message,
+        }
+
+        res.status(status)
+        res.json(response)
+    }
+
+    app.use(Sentry.Handlers.requestHandler())
+    app.use(errorHander)
+}
 
 // -----------------------------------------------------------------------------
 // HTTP Server
 // -----------------------------------------------------------------------------
 
-function runHttpServer() {
-    const port = normalizePort(process.env.PORT || '3000')
+async function runHttpServer() {
+    setupSentry()
 
+    setupBodyParsers()
+    setupLogging()
+    await setupSession()
+
+    setupApiResponseHandlers()
+    setupErrorResponseHandlers()
+
+    const port = normalizePort(process.env.PORT || '3000')
     console.info('Starting HTTP Web Server', `http://localhost:${port}`)
     const server = http.createServer(app)
 
@@ -148,4 +172,4 @@ function runHttpServer() {
     })
 }
 
-runHttpServer()
+runHttpServer().catch(console.error)
--- src/web/components/Global/CodeBlock.vue
+++ src/web/components/Global/CodeBlock.vue
@@ -1,7 +1,6 @@
 <script lang="ts">
 import { ref, computed, defineComponent, watch } from 'vue'
 import hljs from 'highlight.js'
-import 'highlight.js/styles/monokai.css'
 
 export default defineComponent({
     props: {
