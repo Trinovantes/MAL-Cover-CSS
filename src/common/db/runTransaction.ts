c02e74d6460772740880b16961cf0aaeace86277
Stephen Li <stephenli.ca@gmail.com>
2022-05-11 17:12:33 -0400

    Add unit tests for transaction

--- src/common/db/runTransaction.ts
+++ src/common/db/runTransaction.ts
@@ -2,18 +2,46 @@ import { getDbClient } from './client'
 
 export interface Command {
     sql: string
-    params: Record<string, string>
+    params?: Record<string, string>
 }
 
-export type CommandResults = Array<unknown> | undefined
+export type CommandResults = Array<Array<unknown>> | undefined
 
 /**
  * Each command must be 1 statement i.e. only 1 semicolon per string
  */
-export async function runTransaction(commands: Array<Command>): Promise<Array<CommandResults>> {
+export async function runTransaction(commands: Array<Command | string>): Promise<Array<CommandResults>> {
+    const dbClient = await getDbClient()
+    const db = dbClient.getDatabaseInstance()
+
+    const runStatement = (command: Command | string) => new Promise<CommandResults>((resolve, reject) => {
+        const sql = typeof command === 'string'
+            ? command
+            : command.sql
+        const params = typeof command === 'string'
+            ? {}
+            : command.params ?? {}
+
+        db.all(sql, params, function(this, err, rows) {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(rows)
+            }
+        })
+    })
+
+    try {
+        await runStatement('BEGIN;')
+    } catch (err) {
+        console.warn('Transaction Error')
+        console.warn(err)
+
+        throw err
+    }
+
     try {
         const results = []
-        await runStatement('BEGIN;')
 
         for (const command of commands) {
             const result = await runStatement(command)
@@ -25,29 +53,8 @@ export async function runTransaction(commands: Array<Command>): Promise<Array<Co
     } catch (err) {
         console.warn('Transaction Error')
         console.warn(err)
+
         await runStatement('ROLLBACK;')
         throw err
     }
-}
-
-async function runStatement(command: Command | string): Promise<CommandResults> {
-    const dbClient = await getDbClient()
-    const db = dbClient.getDatabaseInstance()
-
-    return new Promise<CommandResults>((resolve, reject) => {
-        const sql = typeof command === 'string'
-            ? command
-            : command.sql
-        const params = typeof command === 'string'
-            ? {}
-            : command.params
-
-        db.all(sql, params, function(this, err, rows) {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(rows)
-            }
-        })
-    })
 }
