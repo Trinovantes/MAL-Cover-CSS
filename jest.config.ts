c0bb238225f35150321e98b79d83ce09c28d825f
Stephen Li <stephenli.ca@gmail.com>
2022-05-10 21:38:22 -0400

    Move jest config to separate file

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from '@jest/types/build/index'
+
+const config: Config.InitialOptions = {
+    bail: true,
+
+    testPathIgnorePatterns: [
+        './tests/e2e/',
+    ],
+
+    setupFiles: [
+        './tests/setup.ts',
+    ],
+}
+
+export default config
