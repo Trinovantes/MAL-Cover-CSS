09a69733d384eff738f03aaf2be2c89503028565
Stephen Li <stephenli.ca@gmail.com>
2022-05-16 03:25:02 -0400

    Validate most recent backup

--- db/backup.sh
+++ db/backup.sh
@@ -4,6 +4,10 @@ echo 'Starting to backup database'
 sqlite3 ./db/live/malcovercss.sqlite3 ".backup './db/backups/$(date --utc +%FT%TZ).sqlite3'"
 echo 'Finished database backup'
 
+echo 'Starting to validate backup'
+find ./db/backups -name "*.sqlite3" -type f -print | sort | tail -n 1 | xargs --verbose -I '%' sqlite3 % 'PRAGMA integrity_check'
+echo 'Finished database validation'
+
 echo 'Starting to delete old backups'
 find ./db/backups -name "*.sqlite3" -type f -print | sort | head -n -30 | xargs --verbose --no-run-if-empty rm
 echo 'Finished deleting old backups'
