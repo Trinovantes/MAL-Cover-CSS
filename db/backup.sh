ea887f6cc6566f39f5f7241b5c0cdd6d2531eb93
Stephen Li <stephenli.ca@gmail.com>
2022-05-16 04:45:43 -0400

    Fix xargs params on alpine

--- db/backup.sh
+++ db/backup.sh
@@ -5,9 +5,11 @@ sqlite3 ./db/live/malcovercss.sqlite3 ".backup './db/backups/$(date --utc +%FT%T
 echo 'Finished database backup'
 
 echo 'Starting to validate backup'
-find ./db/backups -name "*.sqlite3" -type f -print | sort | tail -n 1 | xargs --verbose -I '%' sqlite3 % 'PRAGMA integrity_check'
+find ./db/backups -name "*.sqlite3" -type f -print | sort | tail -n 1 | xargs -t -I '%' sqlite3 % 'PRAGMA integrity_check'
 echo 'Finished database validation'
 
 echo 'Starting to delete old backups'
-find ./db/backups -name "*.sqlite3" -type f -print | sort | head -n -30 | xargs --verbose --no-run-if-empty rm
+# xargs -t: verbose
+# xargs -r: no-run-if-empty
+find -name "*.sqlite3" -type f -print | sort | head -n -30 | xargs -t -r rm
 echo 'Finished deleting old backups'
