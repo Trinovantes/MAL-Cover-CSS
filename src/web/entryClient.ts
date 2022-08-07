2091550e871e8e186164e8ebada981f9b3d3a746
Stephen Li <stephenli.ca@gmail.com>
2022-08-07 19:01:26 -0400

    Skip sentry in dev mode

--- src/web/entryClient.ts
+++ src/web/entryClient.ts
@@ -12,19 +12,24 @@ import { createVueApp } from './createVueApp'
 async function main() {
     const { app, router } = await createVueApp()
 
-    Sentry.init({
-        dsn: SENTRY_DSN,
-        release: DEFINE.GIT_HASH,
-        integrations: [
-            new Integrations.BrowserTracing({
-                routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-            }),
-        ],
-        tracesSampleRate: 0,
-        enabled: !DEFINE.IS_DEV,
-    })
+    if (!DEFINE.IS_DEV) {
+        Sentry.init({
+            app,
+            release: DEFINE.GIT_HASH,
+            dsn: SENTRY_DSN,
+            integrations: [
+                new Integrations.BrowserTracing({
+                    routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+                }),
+            ],
+            tracesSampleRate: 0,
+            enabled: !DEFINE.IS_DEV,
+        })
+    }
 
     app.mount('#app')
 }
 
-main().catch(console.warn)
+window.addEventListener('error', console.warn)
+window.addEventListener('unhandledrejection', console.warn)
+void main()
